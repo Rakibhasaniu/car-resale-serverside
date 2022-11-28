c13069c0eb9632b537347a6cdf459583eba5fa5f
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-28 21:59:04 +0600

    mongodb atlas added

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json())
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@rasel-01.uhpxwkk.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@rasel-01.uhpxwkk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 // async function verifyJWT(req, res, next) {
