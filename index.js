76904130540f2390e46f6b90392b179e337c007f
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-28 21:58:05 +0600

    jwt token and port added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-// const jwt = require('jsonwebtoken');
-// const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
 
 // const app = express();
 // require('dotenv').config();
